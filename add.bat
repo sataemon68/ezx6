6da1afc1d60416ac24d248ed814c224a47fef014
SaTa.(sataemon) <sataemon@gmail.com>
2026-05-17 19:24:53 +0900

    Ver1.12
    Ver.1.12  2026/05/17
                         プラグインRCD(RC-System)関係追加
                         XM6起動後に適切なMIDI音源を選択してください
                         ver.txtでバージョン情報を書くようにした
                         X68用のBAT内容を更新

--- add.bat
+++ add.bat
@@ -1,3 +1,9 @@
-PATH %path%;A:\TL;A:\MX
+echo off
+REM EZX6：このファイルはEZX6システムで使用します最下段に自動実行コマンドを記述できます。
+PATH %path%;A:\TL;A:\MX;A:\RC
 path
-echo MX で MXDRVを組み込めます。
+IF EXIST A:\MX\MX.BAT echo MX で MXDRVを組み込めます。解除はMX -R
+IF EXIST A:\RC\RC.BAT echo RC で RCDを組み込めます。解除はRC -R
+IF EXIST A:\TL\MMDSP.R echo ドライバ常駐後　MMDSPが使用できます。
+REM EZX6：USER COMMAND ↓
+
