85b1a3717a64732f3636132f1a26e997b9a073a3
SaTa.(sataemon) <sataemon@gmail.com>
2026-05-21 20:58:43 +0900

    Ver.1.13
    Ver.1.13  2026/05/21
                         プラグインSTF追加
                         プラグインTwentyOne.x追加(AUTOEXEC.BAT組込)

--- add.bat
+++ add.bat
@@ -1,9 +1,10 @@
 echo off
 REM EZX6：このファイルはEZX6システムで使用します最下段に自動実行コマンドを記述できます。
-PATH %path%;A:\TL;A:\MX;A:\RC
+PATH %path%;A:\TL;A:\MX;A:\RC;A:\TF
 path
 IF EXIST A:\MX\MX.BAT echo MX で MXDRVを組み込めます。解除はMX -R
 IF EXIST A:\RC\RC.BAT echo RC で RCDを組み込めます。解除はRC -R
 IF EXIST A:\TL\MMDSP.R echo ドライバ常駐後　MMDSPが使用できます。
+IF EXIST A:\TF\tf.x echo STF(tf.x)が使用できます。
 REM EZX6：USER COMMAND ↓
 
